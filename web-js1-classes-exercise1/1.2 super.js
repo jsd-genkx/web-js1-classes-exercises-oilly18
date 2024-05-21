76e40ba572d9ea5e7d5d74270712258f22c8d19f
oilly18 <oilly18@hotmail.com>
2024-05-21 13:50:57 +0700

    secondcommit

--- web-js1-classes-exercise1/1.2 super.js
+++ web-js1-classes-exercise1/1.2 super.js
@@ -2,18 +2,85 @@
 // this = this object
 // super = the parent object
 
-class Animal {
+// class Animal {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   // with the super keyword we can extend methods from the parent class
+//   move(speed) {
+//     console.log(`The ${this.name} moves at a speed of ${speed}kph`);
+//   }
+// }
+
+// class Rabbit extends Animal {
+//   constructor(name, age, runSpeed) {
+//     super(name, age);
+//     // this.name = name;
+//     // this.age = age;
+//     this.runSpeed = runSpeed;
+//   }
+
+//   run() {
+//     console.log(`This ${this.name} can run`);
+//     super.move(this.runSpeed);
+//   }
+// }
+
+// class Fish extends Animal {
+//   constructor(name, age, swimSpeed) {
+//     super(name, age);
+//     // this.name = name;
+//     // this.age = age;
+//     this.swimSpeed = swimSpeed;
+//   }
+//   swim() {
+//     console.log(`This ${this.name} can swim`);
+//     super.move(this.swimSpeed);
+//   }
+// }
+
+// class Hawk extends Animal {
+//   constructor(name, age, flySpeed) {
+//     super(name, age);
+//     // this.name = name;
+//     // this.age = age;
+//     this.flySpeed = flySpeed;
+//   }
+//   fly() {
+//     console.log(`This ${this.name} can fly`);
+//     super.move(this.flySpeed);
+//   }
+// }
+
+// // ReferenceError: Must call super constructor in derived class before accessing 'this'
+// const rabbit1 = new Rabbit("rabbit1", 1, 25);
+// const fish1 = new Fish("fish1", 2, 12);
+// const hawk1 = new Hawk("hawk1", 4, 50);
+
+// console.log(`Animal name is ${rabbit1.name}`);
+// console.log(`Animal age is ${rabbit1.age}`);
+// console.log(`Animal speed is ${rabbit1.runSpeed}`);
+
+// // with the super keyword we can extend methods from the parent class
+// fish1.swim();
+
+
+
+
+
+class Friend {
   constructor(name, age) {
     this.name = name;
     this.age = age;
   }
   // with the super keyword we can extend methods from the parent class
   move(speed) {
-    console.log(`The ${this.name} moves at a speed of ${speed}kph`);
+    console.log(`The ${this.name} can move ${speed}kmH`);
   }
 }
 
-class Rabbit extends Animal {
+class John extends Friend {
   constructor(name, age, runSpeed) {
     super(name, age);
     // this.name = name;
@@ -27,7 +94,7 @@ class Rabbit extends Animal {
   }
 }
 
-class Fish extends Animal {
+class Ann extends Friend {
   constructor(name, age, swimSpeed) {
     super(name, age);
     // this.name = name;
@@ -40,27 +107,30 @@ class Fish extends Animal {
   }
 }
 
-class Hawk extends Animal {
+class Jane extends Friend {
   constructor(name, age, flySpeed) {
     super(name, age);
     // this.name = name;
     // this.age = age;
     this.flySpeed = flySpeed;
   }
-  fly() {
-    console.log(`This ${this.name} can fly`);
+  jumpfly() {
+    console.log(`This ${this.name} can jumpfly`);
     super.move(this.flySpeed);
   }
 }
 
 // ReferenceError: Must call super constructor in derived class before accessing 'this'
-const rabbit1 = new Rabbit("rabbit1", 1, 25);
-const fish1 = new Fish("fish1", 2, 12);
-const hawk1 = new Hawk("hawk1", 4, 50);
+const John1 = new John("john1", 12, 100);
+const Ann1 = new Ann("ann1", 22, 80);
+const Jane1 = new Jane("jane1", 14, 90);
 
-console.log(`Animal name is ${rabbit1.name}`);
-console.log(`Animal age is ${rabbit1.age}`);
-console.log(`Animal speed is ${rabbit1.runSpeed}`);
+console.log(`Friend name is ${John1.name}`);
+console.log(`Friend age is ${John1.age}`);
+console.log(`Friend speed is ${John1.runSpeed}`);
 
 // with the super keyword we can extend methods from the parent class
-fish1.swim();
+Ann1.swim();
+Jane1.jumpfly();
+
+
--- web-js1-classes-exercise1/1.3 getter-setter.js
+++ web-js1-classes-exercise1/1.3 getter-setter.js
@@ -3,10 +3,67 @@
 
 // validate and modify a value when reading/writing a property
 
-class Superhero {
-  constructor(name, superpower) {
+// class Superhero {
+//   constructor(name, superpower) {
+//     this._name = name;
+//     this._superpower = superpower;
+//   }
+
+//   // Getter
+//   get name() {
+//     return this._name;
+//   }
+
+//   // Setter
+//   set name(newName) {
+//     if (newName) {
+//       this._name = newName;
+//     } else {
+//       console.log("Name cannot be empty");
+//     }
+//   }
+
+//   // Getter
+//   get superpower() {
+//     return this._superpower;
+//   }
+
+//   // Setter
+//   set superpower(newSuperpower) {
+//     if (newSuperpower) {
+//       this._superpower = newSuperpower;
+//     } else {
+//       console.log("Superpower cannot be empty");
+//     }
+//   }
+
+//   describe() {
+//     console.log(
+//       `I am ${this._name}, and my superpower is ${this._superpower}.`
+//     );
+//   }
+// }
+
+// const thor = new Superhero("Thor", "control over thunder and lightning");
+// thor.describe(); // Output: I am Thor, and my superpower is control over thunder and lightning.
+
+// // Using setters to change properties
+// thor.name = "Thor Odinson";
+// thor.superpower = "godlike strength and control over thunder and lightning";
+// thor.describe(); // Output: I am Thor Odinson, and my superpower is godlike strength and control over thunder and lightning.
+
+
+
+
+
+
+
+
+
+class movies {
+  constructor(name, movietype) {
     this._name = name;
-    this._superpower = superpower;
+    this._movietype = movietype;
   }
 
   // Getter
@@ -24,30 +81,30 @@ class Superhero {
   }
 
   // Getter
-  get superpower() {
-    return this._superpower;
+  get movietype() {
+    return this._movietype;
   }
 
   // Setter
-  set superpower(newSuperpower) {
-    if (newSuperpower) {
-      this._superpower = newSuperpower;
+  set movietype(newmovietype) {
+    if (newmovietype) {
+      this._movietype = newmovietype;
     } else {
-      console.log("Superpower cannot be empty");
+      console.log("Movie type cannot be empty");
     }
   }
 
   describe() {
     console.log(
-      `I am ${this._name}, and my superpower is ${this._superpower}.`
+      `I like ${this._name}, and movie type is ${this._movietype}.`
     );
   }
 }
 
-const thor = new Superhero("Thor", "control over thunder and lightning");
-thor.describe(); // Output: I am Thor, and my superpower is control over thunder and lightning.
+const titanic = new movies("Titanic", "Love Story");
+titanic.describe(); // Output: I am Thor, and my superpower is control over thunder and lightning.
 
 // Using setters to change properties
-thor.name = "Thor Odinson";
-thor.superpower = "godlike strength and control over thunder and lightning";
-thor.describe(); // Output: I am Thor Odinson, and my superpower is godlike strength and control over thunder and lightning.
+titanic.name = "Tatanic";
+titanic.movietype = "Love Story";
+titanic.describe(); // Output: I am Thor Odinson, and my superpower is godlike strength and control over thunder and lightning.
